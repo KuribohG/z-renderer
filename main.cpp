59f81fba35c9eefa4c136212b6c5e645ea441943
Yuheng Zou <zouyuheng1998@gmail.com>
2017-05-18 14:17:13 +0800

    write image to ppm

--- main.cpp
+++ main.cpp
@@ -5,17 +5,33 @@
 #include "material.h"
 #include "scene.h"
 
-const int WIDTH = 100;
-const int HEIGHT = 100;
+const int WIDTH = 500;
+const int HEIGHT = 500;
+
+void write_image_in_ppm(const char *filename, int width, int height, const unsigned char *data) {
+    static unsigned char color[3];
+    FILE *fp = fopen(filename, "wb");
+    fprintf(fp, "P6\n%d %d\n255\n", width, height);
+    int id = 0;
+    for (int j = 0; j < height; j++) {
+        for (int i = 0; i < width; i++) {
+            color[0] = data[id++];
+            color[1] = data[id++];
+            color[2] = data[id++];
+            fwrite(color, 1, 3, fp);
+        }
+    }
+    fclose(fp);
+}
 
 int main() {
-    Camera camera(Vec3(0.0, 0.0, 2.0), Vec3(0.0, 0.0, 0.0), Vec3(0.0, 1.0, 0.0), 3.0, 3.0);
+    Camera camera(Vec3(0.0, 0.0, 2.0), Vec3(0.0, 0.0, 0.0), Vec3(0.0, 1.0, 0.0), 1.5, 1.5);
     camera.initialize();
     Sphere sphere(Vec3(0.0, 0.0, -1.0), 1.0);
     Material material;
     material.set_ambient(0.1);
-    material.set_specular(0.0);
-    material.set_diffuse(1.0);
+    material.set_specular(0.3);
+    material.set_diffuse(0.5);
     material.set_shininess(32.0);
     material.set_color(Vec3(1.0, 0.0, 0.0));
     Scene scene;
@@ -26,16 +42,18 @@ int main() {
     PointLight light(Vec3(-1.0, -1.0, 1.0), 1.0);
     scene.add_light(&light);
 
-    FILE *f = fopen("output.txt", "w");
+    unsigned char *data = new unsigned char[WIDTH * HEIGHT * 3];
+    int id = 0;
     for (int i = 0; i < WIDTH; i++) {
         for (int j = 0; j < HEIGHT; j++) {
             Ray r = camera.get_ray_through_pixel(i, j, WIDTH, HEIGHT);
             Vec3 color = scene.li(r, 5);
-            fprintf(f, "%c ", (char)('a' + (int)(color[0] / 0.1)));
+            data[id++] = (unsigned char)(std::min(color[0], (Float)1.0) * 256);
+            data[id++] = (unsigned char)(std::min(color[1], (Float)1.0) * 256);
+            data[id++] = (unsigned char)(std::min(color[2], (Float)1.0) * 256);
         }
-        fprintf(f, "\n");
     }
-    fclose(f);
+    write_image_in_ppm("image.ppm", WIDTH, HEIGHT, data);
 
     return 0;
 }
